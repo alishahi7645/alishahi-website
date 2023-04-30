29ab7d93f19087640ca255e2c9968ab734dc754b
alishahi <shahy8227@gmail.com>
2023-04-30 13:04:21 -0700

    end styling header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { AiOutlineBars } from "react-icons/ai";
+import { RiCloseLine } from "react-icons/ri";
+import "./Header.css";
 
 function Header() {
+  const [showicon, setShowicon] = useState(false);
+  const togglemenu = () =>{
+    setShowicon(!showicon);
+  }
   return (
     <header id="header" className="shadow p-3">
       <div className="container">
-        <div className="header-item d-flex align-items-center justify-content-space-between">
-          <ul className="d-flex">
-            <li>
-              <a href="">تماس بامن</a>
-            </li>
-            <li>
-              <a href="">درباره من</a>
-            </li>
-            <li>
-              <a href="">خدمات</a>
-            </li>
-            <li>
-              <a href="">نمونه کارها</a>
-            </li>
-          </ul>
-          <h3>alishahi</h3>
-        </div>
+        <Headeritem>
+        <div className="menu-icon" onClick={togglemenu}>
+            {showicon ? (
+              <RiCloseLine cursor={"pointer"} size={35} color="#000" />
+            ) : (
+              <AiOutlineBars size={35} color="#000" cursor={"pointer"} />
+            )}
+          </div>
+          <div className="header-right">
+            <ul id={showicon ? "show-links-mobile" : "show-links-mobile-hide"}>
+              <li>
+                <a href="">تماس بامن</a>
+              </li>
+              <li>
+                <a href="">درباره من</a>
+              </li>
+              <li>
+                <a href="">خدمات</a>
+              </li>
+              <li>
+                <a href="">نمونه کارها</a>
+              </li>
+            </ul>
+          </div>
+          <div className="header-left">
+            <h3>alishahi</h3>
+          </div>
+        </Headeritem>
       </div>
     </header>
   );
 }
 
-const headeritem = styled.div`
+const Headeritem = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-`
+
+  ul {
+    display: flex;
+
+    li {
+      margin-right: 10px;
+      a {
+        text-decoration: none;
+        color: #000;
+        font-weight: 500;
+        font-size: 16px;
+        transition: all 0.5s ease;
+      }
+      a:hover {
+        color: crimson;
+      }
+    }
+  }
+  h3 {
+    color: crimson;
+  }
+`;
 
 export default Header;
