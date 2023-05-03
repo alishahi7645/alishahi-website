b7cf1e2b842a764845106624bd5f7d3fd4ad9d5b
alishahi <shahy8227@gmail.com>
2023-05-03 00:58:52 -0700

    end styling header and changed font project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header.js'
 import Home from './components/Home/Home.js';
 import Portfolio from './components/Portfolio/Portfolio.js';
 import Services from './components/Services/Services.js';
+import './index.css'
 function App() {
   return (
     <div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer py-5 border-top'>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <p className="text-muted m-0 text-center">
+                        &copy;طراحی وتوسعه علی شاهی
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
